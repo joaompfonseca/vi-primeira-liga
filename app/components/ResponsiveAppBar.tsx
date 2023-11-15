6a0973f7f22bb6c2b9427fc2b5c9daa739d45790
João Fonseca <joao.fonseca@ua.pt>
2023-11-15 12:08:44 +0000

    add navbar to season page

---
+++ app/components/ResponsiveAppBar.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import AdbIcon from '@mui/icons-material/Adb';
+import Link from 'next/link';
+
+type ResponsiveAppBarProps = {
+    pages: {label: string, link: string}[],
+}
+export const ResponsiveAppBar = ({pages}: ResponsiveAppBarProps) => {
+    return (
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <AdbIcon sx={{display: {xs: 'none', md: 'flex'}, mr: 1}}/>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="#app-bar-with-responsive-menu"
+                        sx={{
+                            mr: 2,
+                            display: {xs: 'none', md: 'flex'},
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        PrimeiraLiga
+                    </Typography>
+                    <Box sx={{flexGrow: 1, display: {xs: 'none', md: 'flex'}}}>
+                        {pages.map(({label, link}) => (
+                            <Link
+                                key={label}
+                                href={link}>
+                                <Button
+                                    sx={{my: 2, color: 'white', display: 'block'}}
+                                >
+                                    {label}
+                                </Button>
+                            </Link>
+                        ))}
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+}
+
+export default ResponsiveAppBar;
--- app/season/layout.tsx
+++ app/season/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import ResponsiveAppBar from "@/app/components/ResponsiveAppBar";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +16,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ResponsiveAppBar
+            pages={[
+              {label: "Tudo", link: "/"},
+              {label: "Temporada", link: "/season"},
+            ]}
+        />
+        {children}
+      </body>
     </html>
   )
 }
